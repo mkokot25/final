0526552ff3803e98e2bb6c725aa08cf0ba08e269
= <=>
2015-08-06 09:12:50 -0400

    noo

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static( __dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
+app.set('views', '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
