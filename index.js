8be2977bb69e7bd7645b7293323477569465f105
= <=>
2015-08-06 09:35:16 -0400

    hello

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.join(__dirname, 'public')));
